e1b6a8b2e578b8d31a190b8dd1a7be1de0ba1ba0
skalamin11 <skalamin392@gmail.com>
2021-07-31 21:05:45 +0600

    Template created

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// MOBILE MENU ACTIVATION
+
+function openNav(){
+    document.getElementById("menu-overlay").style.opacity="1";
+    document.getElementById("menu-overlay").style.visibility="visible";
+    
+};
+function closeNav(){
+	document.getElementById("menu-overlay").style.opacity="0";
+	document.getElementById("menu-overlay").style.visibility="hidden";
+};
