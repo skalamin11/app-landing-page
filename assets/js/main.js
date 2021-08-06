595546ac0aa5ab7482c69ab438c22de978541f51
skalamin11 <skalamin392@gmail.com>
2021-08-06 22:10:34 +0600

    jQuery cdn link, Sticky header and bottom-to-top button added

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,30 @@ function closeNav(){
 	document.getElementById("menu-overlay").style.opacity="0";
 	document.getElementById("menu-overlay").style.visibility="hidden";
 };
+/* STICKY NAVBAR*/
+$(window).scroll(function(){
+    if($(window).scrollTop() > 0){
+        $('header').addClass('sticky');
+    }
+    else{
+        $('header').removeClass('sticky');
+    }
+});
+
+/* BOTTOM TO TOP BUTTON ACTIVATE*/
+$("i.go-to-top").hide();
+  $(window).on("scroll", function(){
+    	if($(window).scrollTop() > 700){
+    		$("i.go-to-top").show("fast");
+    	}
+      else{
+    		$("i.go-to-top").hide("fast");
+    	}
+    });
+    //GO TOP SCROLL FUNCTIONs
+    $("i.go-to-top").click(function(){
+    	$("html,body").animate({'scrollTop':0 },2000);
+    	return false;
+    });
 /*  WOW.JS ACTIVATION */
 new WOW().init();
